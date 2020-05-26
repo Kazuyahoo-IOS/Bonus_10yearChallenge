b9c48f8527fbd0c2549bd2187427e2fae21d7a39
kazuyahooo <46344542+kazuyahooo@users.noreply.github.com>
2020-05-26 14:56:48 +0800

    Initial Commit

---
+++ Bonus_10yearChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bonus_10yearChallenge
+//
+//  Created by 王瑋 on 2020/5/26.
+//  Copyright © 2020 王瑋. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
